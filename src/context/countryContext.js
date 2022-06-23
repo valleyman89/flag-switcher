e7ed1b81d706ae14ee82c3a1adf9dc4fd9cab943
valleyman89 <dev@stevenrolph.com>
2022-06-23 20:08:13 +0100

    define temporary data call for context

--- src/context/countryContext.js
+++ src/context/countryContext.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useState, createContext } from "react";
+import React, { useState, createContext } from "react";
+import { getCountries } from "../services/data";
 
-// Context
 export const CountryContext = createContext();
 
 export const CountryProvider = ({ children }) => {
-  const [countryData, setCountryData] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://restcountries.com/v3.1/all`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => setCountryData(data))
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+  const [countryData, setCountryData] = useState(getCountries());
 
   return (
     <CountryContext.Provider value={{ countryData, setCountryData }}>
