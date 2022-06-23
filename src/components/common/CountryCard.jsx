eea7da8f2483fdc4f3bbe3db424d63cdfbc50a81
valleyman89 <dev@stevenrolph.com>
2022-06-23 20:07:15 +0100

    add styles and additional data

--- src/components/common/CountryCard.jsx
+++ src/components/common/CountryCard.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
+import { Link } from "react-router-dom";
 
 const CountryCard = ({ data }) => {
   return (
-    <>
-      <Card className="mt-4" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={data.flags.png} />
-        <Card.Body>
-          <Card.Title>{data.name.common}</Card.Title>
-          <Card.Text>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <strong>Population: </strong>
-                {data.population}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Region: </strong>
-                {data.region}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Capital: </strong>
-                {data.capital}
-              </ListGroup.Item>
-            </ListGroup>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </>
+    <Card className="mb-4 shadow" style={{ width: "18rem" }}>
+      <Link to={`/country/${data.cioc}`}>
+        <Card.Img
+          variant="top"
+          src={data.flags.png}
+          alt={`flag of ${data.name.common}`}
+        />
+      </Link>
+      <Card.Body>
+        <Card.Title>{data.name.common}</Card.Title>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <strong>Population: </strong>
+            {data.population.toLocaleString()}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Region: </strong>
+            {data.region}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Capital: </strong>
+            {data.capital}
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+    </Card>
   );
 };
 
