57ec34bcfe5a0f1a2017964e259c7a368fdb7855
valleyman89 <dev@stevenrolph.com>
2022-06-23 20:07:51 +0100

    define temporary data call

---
+++ src/services/data.js
@@ -0,0 +1,547 @@
+let countries = [
+  {
+    name: {
+      common: "Germany",
+      official: "Federal Republic of Germany",
+      nativeName: {
+        deu: {
+          official: "Bundesrepublik Deutschland",
+          common: "Deutschland",
+        },
+      },
+    },
+    tld: [".de"],
+    cca2: "DE",
+    ccn3: "276",
+    cca3: "DEU",
+    cioc: "GER",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+      EUR: {
+        name: "Euro",
+        symbol: "€",
+      },
+    },
+    idd: {
+      root: "+4",
+      suffixes: ["9"],
+    },
+    capital: ["Berlin"],
+    altSpellings: [
+      "DE",
+      "Federal Republic of Germany",
+      "Bundesrepublik Deutschland",
+    ],
+    region: "Europe",
+    subregion: "Western Europe",
+    languages: {
+      deu: "German",
+    },
+    translations: {
+      ara: {
+        official: "جمهورية ألمانيا الاتحادية",
+        common: "ألمانيا",
+      },
+      ces: {
+        official: "Spolková republika Německo",
+        common: "Německo",
+      },
+      cym: {
+        official: "Federal Republic of Germany",
+        common: "Germany",
+      },
+      deu: {
+        official: "Bundesrepublik Deutschland",
+        common: "Deutschland",
+      },
+      est: {
+        official: "Saksamaa Liitvabariik",
+        common: "Saksamaa",
+      },
+      fin: {
+        official: "Saksan liittotasavalta",
+        common: "Saksa",
+      },
+      fra: {
+        official: "République fédérale d'Allemagne",
+        common: "Allemagne",
+      },
+      hrv: {
+        official: "Njemačka Federativna Republika",
+        common: "Njemačka",
+      },
+      hun: {
+        official: "Német Szövetségi Köztársaság",
+        common: "Németország",
+      },
+      ita: {
+        official: "Repubblica federale di Germania",
+        common: "Germania",
+      },
+      jpn: {
+        official: "ドイツ連邦共和国",
+        common: "ドイツ",
+      },
+      kor: {
+        official: "독일 연방 공화국",
+        common: "독일",
+      },
+      nld: {
+        official: "Bondsrepubliek Duitsland",
+        common: "Duitsland",
+      },
+      per: {
+        official: "جمهوری فدرال آلمان",
+        common: "آلمان",
+      },
+      pol: {
+        official: "Republika Federalna Niemiec",
+        common: "Niemcy",
+      },
+      por: {
+        official: "República Federal da Alemanha",
+        common: "Alemanha",
+      },
+      rus: {
+        official: "Федеративная Республика Германия",
+        common: "Германия",
+      },
+      slk: {
+        official: "Nemecká spolková republika",
+        common: "Nemecko",
+      },
+      spa: {
+        official: "República Federal de Alemania",
+        common: "Alemania",
+      },
+      swe: {
+        official: "Förbundsrepubliken Tyskland",
+        common: "Tyskland",
+      },
+      urd: {
+        official: "وفاقی جمہوریہ جرمنی",
+        common: "جرمنی",
+      },
+      zho: {
+        official: "德意志联邦共和国",
+        common: "德国",
+      },
+    },
+    latlng: [51.0, 9.0],
+    landlocked: false,
+    borders: ["AUT", "BEL", "CZE", "DNK", "FRA", "LUX", "NLD", "POL", "CHE"],
+    area: 357114.0,
+    demonyms: {
+      eng: {
+        f: "German",
+        m: "German",
+      },
+      fra: {
+        f: "Allemande",
+        m: "Allemand",
+      },
+    },
+    flag: "🇩🇪",
+    maps: {
+      googleMaps: "https://goo.gl/maps/mD9FBMq1nvXUBrkv6",
+      openStreetMaps: "https://www.openstreetmap.org/relation/51477",
+    },
+    population: 83240525,
+    gini: {
+      2016: 31.9,
+    },
+    fifa: "GER",
+    car: {
+      signs: ["DY"],
+      side: "right",
+    },
+    timezones: ["UTC+01:00"],
+    continents: ["Europe"],
+    flags: {
+      png: "https://flagcdn.com/w320/de.png",
+      svg: "https://flagcdn.com/de.svg",
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/de.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/de.svg",
+    },
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [52.52, 13.4],
+    },
+    postalCode: {
+      format: "#####",
+      regex: "^(\\d{5})$",
+    },
+  },
+  {
+    name: {
+      common: "United Kingdom",
+      official: "United Kingdom of Great Britain and Northern Ireland",
+      nativeName: {
+        eng: {
+          official: "United Kingdom of Great Britain and Northern Ireland",
+          common: "United Kingdom",
+        },
+      },
+    },
+    tld: [".uk"],
+    cca2: "GB",
+    ccn3: "826",
+    cca3: "GBR",
+    cioc: "GBR",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+      GBP: {
+        name: "British pound",
+        symbol: "£",
+      },
+    },
+    idd: {
+      root: "+4",
+      suffixes: ["4"],
+    },
+    capital: ["London"],
+    altSpellings: ["GB", "UK", "Great Britain"],
+    region: "Europe",
+    subregion: "Northern Europe",
+    languages: {
+      eng: "English",
+    },
+    translations: {
+      ara: {
+        official: "المملكة المتحدة لبريطانيا العظمى وايرلندا الشمالية",
+        common: "المملكة المتحدة",
+      },
+      ces: {
+        official: "Spojené království Velké Británie a Severního Irska",
+        common: "Spojené království",
+      },
+      cym: {
+        official: "United Kingdom of Great Britain and Northern Ireland",
+        common: "United Kingdom",
+      },
+      deu: {
+        official: "Vereinigtes Königreich Großbritannien und Nordirland",
+        common: "Vereinigtes Königreich",
+      },
+      est: {
+        official: "Suurbritannia ja Põhja-Iiri Ühendkuningriik",
+        common: "Suurbritannia",
+      },
+      fin: {
+        official:
+          "Ison-Britannian ja Pohjois-Irlannin yhdistynyt kuningaskunta",
+        common: "Yhdistynyt kuningaskunta",
+      },
+      fra: {
+        official: "Royaume-Uni de Grande-Bretagne et d'Irlande du Nord",
+        common: "Royaume-Uni",
+      },
+      hrv: {
+        official: "Ujedinjeno Kraljevstvo Velike Britanije i Sjeverne Irske",
+        common: "Ujedinjeno Kraljevstvo",
+      },
+      hun: {
+        official: "Nagy-Britannia és Észak-Írország Egyesült Királysága",
+        common: "Egyesült Királyság",
+      },
+      ita: {
+        official: "Regno Unito di Gran Bretagna e Irlanda del Nord",
+        common: "Regno Unito",
+      },
+      jpn: {
+        official: "グレート·ブリテンおよび北アイルランド連合王国",
+        common: "イギリス",
+      },
+      kor: {
+        official: "그레이트브리튼 북아일랜드 연합 왕국",
+        common: "영국",
+      },
+      nld: {
+        official: "Verenigd Koninkrijk van Groot-Brittannië en Noord-Ierland",
+        common: "Verenigd Koninkrijk",
+      },
+      per: {
+        official: "پادشاهی متحد بریتانیای کبیر و ایرلند شمالی",
+        common: "انگلیس",
+      },
+      pol: {
+        official:
+          "Zjednoczone Królestwo Wielkiej Brytanii i Irlandii Północnej",
+        common: "Zjednoczone Krłlestwo",
+      },
+      por: {
+        official: "Reino Unido da Grã-Bretanha e Irlanda do Norte",
+        common: "Reino Unido",
+      },
+      rus: {
+        official: "Соединенное Королевство Великобритании и Северной Ирландии",
+        common: "Великобритания",
+      },
+      slk: {
+        official: "Spojené kráľovstvo Veľkej Británie a SevernéhoÌrska",
+        common: "Veľká Británia (Spojené kráľovstvo)",
+      },
+      spa: {
+        official: "Reino Unido de Gran Bretaña e Irlanda del Norte",
+        common: "Reino Unido",
+      },
+      swe: {
+        official: "Förenade konungariket Storbritannien och Nordirland",
+        common: "Storbritannien",
+      },
+      urd: {
+        official: "مملکتِ متحدہ برطانیہ عظمی و شمالی آئرلینڈ",
+        common: "مملکتِ متحدہ",
+      },
+      zho: {
+        official: "大不列颠及北爱尔兰联合王国",
+        common: "英国",
+      },
+    },
+    latlng: [54.0, -2.0],
+    landlocked: false,
+    borders: ["IRL"],
+    area: 242900.0,
+    demonyms: {
+      eng: {
+        f: "British",
+        m: "British",
+      },
+      fra: {
+        f: "Britannique",
+        m: "Britannique",
+      },
+    },
+    flag: "🇬🇧",
+    maps: {
+      googleMaps: "https://goo.gl/maps/FoDtc3UKMkFsXAjHA",
+      openStreetMaps: "https://www.openstreetmap.org/relation/62149",
+    },
+    population: 67215293,
+    gini: {
+      2017: 35.1,
+    },
+    car: {
+      signs: ["GB"],
+      side: "left",
+    },
+    timezones: [
+      "UTC-08:00",
+      "UTC-05:00",
+      "UTC-04:00",
+      "UTC-03:00",
+      "UTC-02:00",
+      "UTC",
+      "UTC+01:00",
+      "UTC+02:00",
+      "UTC+06:00",
+    ],
+    continents: ["Europe"],
+    flags: {
+      png: "https://flagcdn.com/w320/gb.png",
+      svg: "https://flagcdn.com/gb.svg",
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/gb.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/gb.svg",
+    },
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [51.5, -0.08],
+    },
+    postalCode: {
+      format: "@# #@@|@## #@@|@@# #@@|@@## #@@|@#@ #@@|@@#@ #@@|GIR0AA",
+      regex:
+        "^(([A-Z]\\d{2}[A-Z]{2})|([A-Z]\\d{3}[A-Z]{2})|([A-Z]{2}\\d{2}[A-Z]{2})|([A-Z]{2}\\d{3}[A-Z]{2})|([A-Z]\\d[A-Z]\\d[A-Z]{2})|([A-Z]{2}\\d[A-Z]\\d[A-Z]{2})|(GIR0AA))$",
+    },
+  },
+  {
+    name: {
+      common: "Ireland",
+      official: "Republic of Ireland",
+      nativeName: {
+        eng: {
+          official: "Republic of Ireland",
+          common: "Ireland",
+        },
+        gle: {
+          official: "Poblacht na hÉireann",
+          common: "Éire",
+        },
+      },
+    },
+    tld: [".ie"],
+    cca2: "IE",
+    ccn3: "372",
+    cca3: "IRL",
+    cioc: "IRL",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+      EUR: {
+        name: "Euro",
+        symbol: "€",
+      },
+    },
+    idd: {
+      root: "+3",
+      suffixes: ["53"],
+    },
+    capital: ["Dublin"],
+    altSpellings: ["IE", "Éire", "Republic of Ireland", "Poblacht na hÉireann"],
+    region: "Europe",
+    subregion: "Northern Europe",
+    languages: {
+      eng: "English",
+      gle: "Irish",
+    },
+    translations: {
+      ara: {
+        official: "جمهورية أيرلندا",
+        common: "أيرلندا",
+      },
+      ces: {
+        official: "Irsko",
+        common: "Irsko",
+      },
+      cym: {
+        official: "Republic of Ireland",
+        common: "Ireland",
+      },
+      deu: {
+        official: "Republik Irland",
+        common: "Irland",
+      },
+      est: {
+        official: "Iirimaa",
+        common: "Iirimaa",
+      },
+      fin: {
+        official: "Irlannin tasavalta",
+        common: "Irlanti",
+      },
+      fra: {
+        official: "République d'Irlande",
+        common: "Irlande",
+      },
+      hrv: {
+        official: "Republika Irska",
+        common: "Irska",
+      },
+      hun: {
+        official: "Ír Köztársaság",
+        common: "Írország",
+      },
+      ita: {
+        official: "Repubblica d'Irlanda",
+        common: "Irlanda",
+      },
+      jpn: {
+        official: "アイルランド共和国",
+        common: "アイルランド",
+      },
+      kor: {
+        official: "아일랜드 공화국",
+        common: "아일랜드",
+      },
+      nld: {
+        official: "Republic of Ireland",
+        common: "Ierland",
+      },
+      per: {
+        official: "ایرلند",
+        common: "ایرلند",
+      },
+      pol: {
+        official: "Republika Irlandii",
+        common: "Irlandia",
+      },
+      por: {
+        official: "República da Irlanda",
+        common: "Irlanda",
+      },
+      rus: {
+        official: "Ирландия",
+        common: "Ирландия",
+      },
+      slk: {
+        official: "Írska republika",
+        common: "Írsko",
+      },
+      spa: {
+        official: "República de Irlanda",
+        common: "Irlanda",
+      },
+      swe: {
+        official: "Irland",
+        common: "Irland",
+      },
+      urd: {
+        official: "جمہوریہ جزیرہ آئرلینڈ",
+        common: "جزیرہ آئرلینڈ",
+      },
+      zho: {
+        official: "爱尔兰共和国",
+        common: "爱尔兰",
+      },
+    },
+    latlng: [53.0, -8.0],
+    landlocked: false,
+    borders: ["GBR"],
+    area: 70273.0,
+    demonyms: {
+      eng: {
+        f: "Irish",
+        m: "Irish",
+      },
+      fra: {
+        f: "Irlandaise",
+        m: "Irlandais",
+      },
+    },
+    flag: "🇮🇪",
+    maps: {
+      googleMaps: "https://goo.gl/maps/hxd1BKxgpchStzQC6",
+      openStreetMaps: "https://www.openstreetmap.org/relation/62273",
+    },
+    population: 4994724,
+    gini: {
+      2017: 31.4,
+    },
+    fifa: "IRL",
+    car: {
+      signs: ["IRL"],
+      side: "left",
+    },
+    timezones: ["UTC"],
+    continents: ["Europe"],
+    flags: {
+      png: "https://flagcdn.com/w320/ie.png",
+      svg: "https://flagcdn.com/ie.svg",
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/ie.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/ie.svg",
+    },
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [53.32, -6.23],
+    },
+  },
+];
+
+export function getCountries() {
+  return countries;
+}
+export function getCountry(code) {
+  return countries.find((country) => country.cioc === code);
+}
+
+// https://restcountries.com/v3.1/all
