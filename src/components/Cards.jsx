8e72bdc54df433707e0e6272d3f27c25b460ca38
valleyman89 <dev@stevenrolph.com>
2022-06-23 20:06:34 +0100

    remove unncessary unordered list

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import CountryCard from "./common/CountryCard";
 import { useCountryContext } from "../context/countryContext";
+import { Container } from "react-bootstrap";
+
 const Cards = () => {
   const { countryData } = useCountryContext();
   return (
-    <div>
-      <ul>
-        {countryData.map((country, index) => (
-          <CountryCard key={index} data={country} />
-        ))}
-      </ul>
-    </div>
+    <Container fluid>
+      {countryData.map((country, index) => (
+        <CountryCard key={index} data={country} />
+      ))}
+    </Container>
   );
 };
 
