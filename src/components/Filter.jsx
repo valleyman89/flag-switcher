15fa7d22427934f88a7a1e776622cf91c269b32a
valleyman89 <dev@stevenrolph.com>
2022-06-24 16:14:37 +0100

    tidy

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -13,33 +13,27 @@ const Filter = () => {
   };
 
   return (
-    <>
-      <Dropdown className="mb-4">
-        <Dropdown.Toggle variant="light" size="lg" id="dropdown-basic">
-          Filter by Region
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {["Africa", "Asia", "America", "Europe", "Oceania"].map((region) => (
-            <Dropdown.Item
-              as="button"
-              id={region}
-              key={region}
-              onClick={(e) => handleFilter(e)}
-            >
-              {region}
-            </Dropdown.Item>
-          ))}
-          <Dropdown.Divider />
+    <Dropdown className="mb-4">
+      <Dropdown.Toggle variant="light" size="lg" id="dropdown-basic">
+        Filter by Region
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {["Africa", "Asia", "America", "Europe", "Oceania"].map((region) => (
           <Dropdown.Item
             as="button"
-            id="Clear"
+            id={region}
+            key={region}
             onClick={(e) => handleFilter(e)}
           >
-            Clear
+            {region}
           </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </>
+        ))}
+        <Dropdown.Divider />
+        <Dropdown.Item as="button" id="Clear" onClick={(e) => handleFilter(e)}>
+          Clear
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
