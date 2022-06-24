99424ba9cb837f06e375ee318856cfd687cd4ba3
valleyman89 <dev@stevenrolph.com>
2022-06-24 17:56:48 +0100

    add responsiveness to cards layout

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import CountryCard from "./common/CountryCard";
 import { useCountryContext } from "../context/countryContext";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
 const Cards = () => {
   const { countryData } = useCountryContext();
   return (
     <Container fluid>
-      {countryData.map((country, index) => (
-        <CountryCard key={index} data={country} />
-      ))}
+      <Row>
+        {countryData.map((country, index) => (
+          <CountryCard key={index} data={country} />
+        ))}
+      </Row>
     </Container>
   );
 };
--- src/components/common/CountryCard.jsx
+++ src/components/common/CountryCard.jsx
@@ -1,36 +1,38 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
 
 const CountryCard = ({ data }) => {
   return (
-    <Card className="mb-4 shadow" style={{ width: "18rem" }}>
-      <Link to={`/country/${data.cioc}`}>
-        <Card.Img
-          variant="top"
-          src={data.flags.png}
-          alt={`flag of ${data.name.common}`}
-        />
-      </Link>
-      <Card.Body>
-        <Card.Title>{data.name.common}</Card.Title>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <strong>Population: </strong>
-            {data.population.toLocaleString()}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <strong>Region: </strong>
-            {data.region}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <strong>Capital: </strong>
-            {data.capital}
-          </ListGroup.Item>
-        </ListGroup>
-      </Card.Body>
-    </Card>
+    <Col>
+      <Card className="mb-4 shadow" style={{ width: "18rem" }}>
+        <Link to={`/country/${data.cioc}`}>
+          <Card.Img
+            variant="top"
+            src={data.flags.png}
+            alt={`flag of ${data.name.common}`}
+          />
+        </Link>
+        <Card.Body>
+          <Card.Title>{data.name.common}</Card.Title>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <strong>Population: </strong>
+              {data.population.toLocaleString()}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Region: </strong>
+              {data.region}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Capital: </strong>
+              {data.capital}
+            </ListGroup.Item>
+          </ListGroup>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
