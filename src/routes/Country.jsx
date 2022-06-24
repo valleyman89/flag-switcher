55c21d7458bbe5eb06e4792fae3eebac0ff3483a
valleyman89 <dev@stevenrolph.com>
2022-06-24 19:33:54 +0100

    adjust responsiveness for large breakpoints

--- src/routes/Country.jsx
+++ src/routes/Country.jsx
@@ -24,60 +24,67 @@ export default function Country() {
           </Link>
         </Col>
       </Row>
-      <Card border="light" className="text-left">
-        <Card.Body>
-          <Card.Img
-            className="mb-4"
-            variant="top"
-            src={country.flags.png}
-            alt={`flag of ${country.name.common}`}
-          />
-          <Card.Title className="mb-4">{country.name.common}</Card.Title>
-          <ListGroup className="mb-4" variant="flush">
-            <ListGroup.Item>
-              <strong>Native Name:</strong>{" "}
-              {Object.keys(country.name.nativeName).map((nativeName, index) => (
-                <span key={index}>{nativeName}</span>
+      <Row>
+        <Col lg="auto">
+          <Card border="light" className="text-left mb-4">
+            <Card.Body>
+              <Card.Img
+                className="mb-4"
+                variant="top"
+                src={country.flags.png}
+                alt={`flag of ${country.name.common}`}
+              />
+              <Card.Title className="mb-4">{country.name.common}</Card.Title>
+              <ListGroup className="mb-4" variant="flush">
+                <ListGroup.Item>
+                  <strong>Native Name:</strong>{" "}
+                  {Object.keys(country.name.nativeName).map(
+                    (nativeName, index) => (
+                      <span key={index}>{nativeName}</span>
+                    )
+                  )}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Population:</strong>{" "}
+                  {country.population.toLocaleString()}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Region:</strong> {country.region}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Sub-region:</strong> {country.subregion}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Capital:</strong> {country.capital}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Top Level Domain:</strong> {country.tld}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Currencies:</strong>{" "}
+                  {Object.keys(country.currencies).map((currency, index) => (
+                    <span key={index}>{currency}</span>
+                  ))}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Languages:</strong> {/* TODO: seperate by comma */}
+                  {Object.keys(country.languages).map((language, index) => (
+                    <span key={index}>{language}</span>
+                  ))}
+                </ListGroup.Item>
+              </ListGroup>
+              <Card.Title>Border Countries:</Card.Title>
+              {country.borders.map((border, index) => (
+                <Link key={index} to={`/country/${border}`}>
+                  <Button className="m-1 shadow" variant="light">
+                    {border}
+                  </Button>
+                </Link>
               ))}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Population:</strong> {country.population.toLocaleString()}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Region:</strong> {country.region}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Sub-region:</strong> {country.subregion}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Capital:</strong> {country.capital}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Top Level Domain:</strong> {country.tld}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Currencies:</strong>{" "}
-              {Object.keys(country.currencies).map((currency, index) => (
-                <span key={index}>{currency}</span>
-              ))}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Languages:</strong> {/* TODO: seperate by comma */}
-              {Object.keys(country.languages).map((language, index) => (
-                <span key={index}>{language}</span>
-              ))}
-            </ListGroup.Item>
-          </ListGroup>
-          <Card.Title>Border Countries:</Card.Title>
-          {country.borders.map((border, index) => (
-            <Link key={index} to={`/country/${border}`}>
-              <Button className="m-1 shadow" variant="light">
-                {border}
-              </Button>
-            </Link>
-          ))}
-        </Card.Body>
-      </Card>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
