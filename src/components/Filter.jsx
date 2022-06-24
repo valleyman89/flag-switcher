3d43d94ee4336a1f8d8c7a3f485573398392f905
valleyman89 <dev@stevenrolph.com>
2022-06-24 12:57:32 +0100

    add filter region logic

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useCountryContext } from "../context/countryContext";
+import { filterCountry, getCountries } from "../services/data";
 
 const Filter = () => {
+  const { setCountryData } = useCountryContext();
+
+  const handleFilter = (e) => {
+    e.target.id === "Clear"
+      ? setCountryData(getCountries())
+      : setCountryData(filterCountry(e.target.id));
+  };
+
   return (
     <>
       <Dropdown className="mb-4">
         <Dropdown.Toggle variant="light" size="lg" id="dropdown-basic">
           Filter by Region
         </Dropdown.Toggle>
-
         <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Africa</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">America</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Asia</Dropdown.Item>
-          <Dropdown.Item href="">Europe</Dropdown.Item>
-          <Dropdown.Item href="">Oceania</Dropdown.Item>
+          {["Africa", "Asia", "America", "Europe", "Oceania"].map((region) => (
+            <Dropdown.Item
+              as="button"
+              id={region}
+              key={region}
+              onClick={(e) => handleFilter(e)}
+            >
+              {region}
+            </Dropdown.Item>
+          ))}
+          <Dropdown.Divider />
+          <Dropdown.Item
+            as="button"
+            id="Clear"
+            onClick={(e) => handleFilter(e)}
+          >
+            Clear
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </>
