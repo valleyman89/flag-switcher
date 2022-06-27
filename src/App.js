a25102111a225d46126bda8bf6bebf276e05a5b5
valleyman89 <dev@stevenrolph.com>
2022-06-27 15:00:57 +0100

    adjust navbar call

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import Search from "./components/Search";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
-import { Alert, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Alert, Container, Row, Col } from "react-bootstrap";
 import { useCountryContext } from "./context/countryContext";
 
 function App() {
   const { countries, loading } = useCountryContext();
   return (
     <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <h1>Where in the world?</h1>
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>Dark Mode</Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Container fluid>
         <Row>
           <Col>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CountryProvider } from "./context/countryContext";
-import { Navbar, Container } from "react-bootstrap";
 import App from "./App";
 import Country from "./routes/Country";
 import React from "react";
@@ -9,24 +8,13 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CountryProvider>
-        <Navbar bg="light">
-          <Container>
-            <Navbar.Brand href="/">
-              <h1>Where in the world?</h1>
-            </Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>Dark Mode</Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/country/:countryCode" element={<Country />} />
-        </Routes>
-      </CountryProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <CountryProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/country" element={<App />} />
+        <Route path="/country/:countryCode" element={<Country />} />
+      </Routes>
+    </CountryProvider>
+  </BrowserRouter>
 );
