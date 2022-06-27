40e4557c54af136254d4bf0ab70c666102cb33d9
valleyman89 <dev@stevenrolph.com>
2022-06-27 20:56:28 +0100

    update langauges, native name, add in navbar

--- src/routes/Country.jsx
+++ src/routes/Country.jsx
@@ -11,106 +11,120 @@ import {
 } from "react-bootstrap";
 import { useCountryContext } from "../context/countryContext";
 import React, { useEffect } from "react";
+import Navigation from "../components/Navigation";
 
 export default function Country() {
   const { capitalise, singleCountry } = useCountryContext();
+  const params = useParams();
   const country = singleCountry(params.countryCode);
   const location = useLocation();
-  const params = useParams();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
   return (
-    <Container>
-      {!country ? (
-        <Alert variant="info">loading...</Alert>
-      ) : (
-        <>
-          <Row>
-            <Col>
-              <Link to="/">
-                <Button className="mt-4 mb-4" variant="light">
-                  Back
-                </Button>
-              </Link>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg="auto">
-              <Card border="light" className="text-left mb-4">
-                <Card.Body>
-                  <Card.Img
-                    className="mb-4"
-                    variant="top"
-                    src={country.flags.png}
-                    alt={`flag of ${country.name.common}`}
-                  />
-                  <Card.Title className="mb-4">
-                    {country.name.common}
-                  </Card.Title>
-                  <ListGroup className="mb-4" variant="flush">
-                    <ListGroup.Item>
-                      <strong>Native Name: </strong>
-                      {Object.keys(country.name.nativeName).map(
-                        (nativeName, index) => (
-                          <span key={index}>{nativeName}</span>
-                        )
-                      )}
-                    </ListGroup.Item>
-                    {[
-                      "capital",
-                      "population",
-                      "region",
-                      "subregion",
-                      "tld",
-                    ].map((fact, index) => (
-                      <React.Fragment key={index}>
-                        {country[fact] ? (
-                          <ListGroup.Item>
-                            <strong>{capitalise(fact)}: </strong>
-                            {fact === "population"
-                              ? country[fact].toLocaleString()
-                              : country[fact]}
-                          </ListGroup.Item>
-                        ) : null}
-                      </React.Fragment>
-                    ))}
-                    <ListGroup.Item>
-                      <strong>Currencies: </strong>
-                      {Object.keys(country.currencies).map(
-                        (currency, index) => (
-                          <span key={index}>{currency}</span>
-                        )
-                      )}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <strong>Languages: </strong>
-                      {Object.keys(country.languages).map((language, index) => (
-                        <Badge key={index} pill bg="light" text="dark">
-                          {language}
-                        </Badge>
+    <>
+      <Navigation />
+      <Container>
+        {!country ? (
+          <Alert variant="info">loading...</Alert>
+        ) : (
+          <>
+            <Row>
+              <Col>
+                <Link to="/">
+                  <Button className="mt-4 mb-4" variant="light">
+                    Back
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="auto">
+                <Card border="light" className="text-left mb-4">
+                  <Card.Body>
+                    <Card.Img
+                      className="mb-4"
+                      variant="top"
+                      src={country.flags.png}
+                      alt={`flag of ${country.name.common}`}
+                    />
+                    <Card.Title className="mb-4">
+                      {country.name.common}
+                    </Card.Title>
+                    <ListGroup className="mb-4" variant="flush">
+                      <ListGroup.Item>
+                        <strong>Native Name: </strong>
+                        {Object.keys(country.name.nativeName).map(
+                          (nativeName, index) => (
+                            <h5 key={index} style={{ display: "inline" }}>
+                              <Badge pill bg="light" text="dark">
+                                {country.name.nativeName[nativeName].common}
+                              </Badge>
+                            </h5>
+                          )
+                        )}
+                      </ListGroup.Item>
+                      {[
+                        "capital",
+                        "population",
+                        "region",
+                        "subregion",
+                        "tld",
+                      ].map((fact, index) => (
+                        <React.Fragment key={index}>
+                          {country[fact] ? (
+                            <ListGroup.Item>
+                              <strong>{capitalise(fact)}: </strong>
+                              {fact === "population"
+                                ? country[fact].toLocaleString()
+                                : country[fact]}
+                            </ListGroup.Item>
+                          ) : null}
+                        </React.Fragment>
                       ))}
-                    </ListGroup.Item>
-                  </ListGroup>
-                  {country.borders ? (
-                    <Card.Title>Border Countries: </Card.Title>
-                  ) : null}
-                  {country.borders &&
-                    country.borders.map((border, index) => (
-                      <Link key={index} to={`/country/${border}`}>
-                        <Button className="m-1 shadow" variant="light">
-                          {border}
-                        </Button>
-                      </Link>
-                    ))}
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </>
-      )}
-    </Container>
+                      <ListGroup.Item>
+                        <strong>Currencies: </strong>
+                        {Object.keys(country.currencies).map(
+                          (currency, index) => (
+                            <span key={index}>
+                              {country.currencies[currency].name}
+                            </span>
+                          )
+                        )}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <strong>Languages: </strong>
+                        {Object.keys(country.languages).map(
+                          (language, index) => (
+                            <h5 key={index} style={{ display: "inline" }}>
+                              <Badge pill bg="light" text="dark">
+                                {country.languages[language]}
+                              </Badge>
+                            </h5>
+                          )
+                        )}
+                      </ListGroup.Item>
+                    </ListGroup>
+                    {country.borders ? (
+                      <Card.Title>Border Countries: </Card.Title>
+                    ) : null}
+                    {country.borders &&
+                      country.borders.map((border, index) => (
+                        <Link key={index} to={`/country/${border}`}>
+                          <Button className="m-1 shadow" variant="light">
+                            {border}
+                          </Button>
+                        </Link>
+                      ))}
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </>
+        )}
+      </Container>
+    </>
   );
 }
