5ee52333d352e438a5396f8ae39763e046f4fc61
valleyman89 <dev@stevenrolph.com>
2022-06-28 10:45:48 +0100

    remove card component to allow for responsiveness

--- src/routes/Country.jsx
+++ src/routes/Country.jsx
@@ -3,9 +3,9 @@ import {
   Alert,
   Badge,
   Button,
-  Card,
   Col,
   Container,
+  Image,
   ListGroup,
   Row,
 } from "react-bootstrap";
@@ -47,88 +47,70 @@ export default function Country() {
             </Row>
             <Row>
               <Col lg="auto">
-                <Card border="light" className="text-left mb-4">
-                  <Card.Body>
-                    <Card.Img
-                      className="mb-4"
-                      variant="top"
-                      src={country.flags.png}
-                      alt={`flag of ${country.name.common}`}
-                    />
-                    <Card.Title className="mb-4">
-                      {country.name.common}
-                    </Card.Title>
-                    <ListGroup className="mb-4" variant="flush">
-                      <ListGroup.Item>
-                        <strong>Native Name: </strong>
-                        {Object.keys(country.name.nativeName).map(
-                          (nativeName, index) => (
-                            <h5 key={index} style={{ display: "inline" }}>
-                              <Badge pill bg="light" text="dark">
-                                {country.name.nativeName[nativeName].common}
-                              </Badge>
-                            </h5>
-                          )
-                        )}
-                      </ListGroup.Item>
-                      {[
-                        "capital",
-                        "population",
-                        "region",
-                        "subregion",
-                        "tld",
-                      ].map((fact, index) => (
-                        <React.Fragment key={index}>
-                          {country[fact] ? (
-                            <ListGroup.Item>
-                              <strong>{capitalise(fact)}: </strong>
-                              {fact === "population"
-                                ? country[fact].toLocaleString()
-                                : country[fact]}
-                            </ListGroup.Item>
-                          ) : null}
-                        </React.Fragment>
-                      ))}
-                      <ListGroup.Item>
-                        <strong>Currencies: </strong>
-                        {Object.keys(country.currencies).map(
-                          (currency, index) => (
-                            <span key={index}>
-                              {country.currencies[currency].name}
-                            </span>
-                          )
-                        )}
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <strong>Languages: </strong>
-                        {Object.keys(country.languages).map(
-                          (language, index) => (
-                            <h5 key={index} style={{ display: "inline" }}>
-                              <Badge pill bg="light" text="dark">
-                                {country.languages[language]}
-                              </Badge>
-                            </h5>
-                          )
-                        )}
-                      </ListGroup.Item>
-                    </ListGroup>
-                    {country.borders ? (
-                      <Card.Title>Border Countries: </Card.Title>
-                    ) : null}
-                    {country.borders &&
-                      country.borders.map((border, index) => (
-                        <Link key={index} to={`/country/${border}`}>
-                          <Button
-                            size="lg"
-                            className="m-1 shadow"
-                            variant="light"
-                          >
-                            {displayFlag(border)}
-                          </Button>
-                        </Link>
-                      ))}
-                  </Card.Body>
-                </Card>
+                <Image
+                  className="mb-4"
+                  variant="top"
+                  src={country.flags.png}
+                  alt={`flag of ${country.name.common}`}
+                />
+              </Col>
+              <Col lg="auto">
+                <h1>{country.name.common}</h1>
+                <ListGroup className="mb-4" variant="flush">
+                  <ListGroup.Item>
+                    <strong>Native Name: </strong>
+                    {Object.keys(country.name.nativeName).map(
+                      (nativeName, index) => (
+                        <h5 key={index} style={{ display: "inline" }}>
+                          <Badge pill bg="light" text="dark">
+                            {country.name.nativeName[nativeName].common}
+                          </Badge>
+                        </h5>
+                      )
+                    )}
+                  </ListGroup.Item>
+                  {["capital", "population", "region", "subregion", "tld"].map(
+                    (fact, index) => (
+                      <React.Fragment key={index}>
+                        {country[fact] ? (
+                          <ListGroup.Item>
+                            <strong>{capitalise(fact)}: </strong>
+                            {fact === "population"
+                              ? country[fact].toLocaleString()
+                              : country[fact]}
+                          </ListGroup.Item>
+                        ) : null}
+                      </React.Fragment>
+                    )
+                  )}
+                  <ListGroup.Item>
+                    <strong>Currencies: </strong>
+                    {Object.keys(country.currencies).map((currency, index) => (
+                      <span key={index}>
+                        {country.currencies[currency].name}
+                      </span>
+                    ))}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <strong>Languages: </strong>
+                    {Object.keys(country.languages).map((language, index) => (
+                      <h5 key={index} style={{ display: "inline" }}>
+                        <Badge pill bg="light" text="dark">
+                          {country.languages[language]}
+                        </Badge>
+                      </h5>
+                    ))}
+                  </ListGroup.Item>
+                </ListGroup>
+                {country.borders ? <h2>Border Countries: </h2> : null}
+                {country.borders &&
+                  country.borders.map((border, index) => (
+                    <Link key={index} to={`/country/${border}`}>
+                      <Button size="lg" className="m-1 shadow" variant="light">
+                        {displayFlag(border)}
+                      </Button>
+                    </Link>
+                  ))}
               </Col>
             </Row>
           </>
